46e80d33f74efd570a85d61a5cae33adfe3651a9
HSA2000 <hsateam@gmail.com>
2014-07-19 05:24:17 +0200

    Update message.py
    For total translations to any language...

--- plugin/controllers/models/message.py
+++ plugin/controllers/models/message.py
@@ -8,41 +8,37 @@
 #               published by the Free Software Foundation.                   #
 #                                                                            #
 ##############################################################################
+# 2014.07.15 01:23:51 CEST
+#Embedded file name: usr/lib/enigma2/python/Plugins/Extensions/OpenWebif/controllers/models/message.py
+from Plugins.Extensions.OpenWebif.__init__ import _
 from Screens.MessageBox import MessageBox
-
 lastreply = None
 
 def messageReply(reply):
-	global lastreply
-	lastreply = reply
+    global lastreply
+    lastreply = reply
+
 
 def sendMessage(session, message, ttype, timeout):
-	global lastreply
-	if ttype not in [MessageBox.TYPE_YESNO, MessageBox.TYPE_INFO, MessageBox.TYPE_WARNING, MessageBox.TYPE_ERROR]:
-		ttype = MessageBox.TYPE_INFO
+    global lastreply
+    if ttype not in [MessageBox.TYPE_YESNO,
+     MessageBox.TYPE_INFO,
+     MessageBox.TYPE_WARNING,
+     MessageBox.TYPE_ERROR]:
+        ttype = MessageBox.TYPE_INFO
+    if ttype == MessageBox.TYPE_YESNO:
+        lastreply = None
+        session.openWithCallback(messageReply, MessageBox, message, type=ttype, timeout=timeout)
+    else:
+        session.open(MessageBox, message, type=ttype, timeout=timeout)
+    return {'result': True,
+     'message': _('Message sent successfully!')}
 
-	if ttype == MessageBox.TYPE_YESNO:
-		lastreply = None
-		session.openWithCallback(messageReply, MessageBox, message, type=ttype, timeout=timeout)
-	else:
-		session.open(MessageBox, message, type=ttype, timeout=timeout)
-
-	return {
-		"result": True,
-		"message": "Message sent successfully!"
-	}
 
 def getMessageAnswer():
-	global lastreply
-	reply = lastreply
-
-	if reply is None:
-		return {
-			"result": False,
-			"message": "No answer in time"
-		}
-
-	return {
-		"result": True,
-		"message": "Answer is YES!" if reply else "Answer is NO!"
-	}
+    reply = lastreply
+    if reply is None:
+        return {'result': False,
+         'message': _('No answer in time')}
+    return {'result': True,
+     'message': _('Answer is YES!') if reply else _('Answer is NO!')}
